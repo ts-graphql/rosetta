47fd293b7c37369c02cf2f07bc97d90bcf440f1a
stephentuso <tusodev@gmail.com>
2019-03-23 19:11:15 -0400

    test: increase timeout for type tests

--- src/__tests__/types.test.ts
+++ src/__tests__/types.test.ts
@@ -5,12 +5,12 @@ describe('types', () => {
   describe('invalid files', () => {
     it('should have type errors', async () => {
       await typeCheckFolder([__dirname, 'invalid'], true);
-    });
+    }, 30000);
   });
 
   describe('valid files', () => {
     it('should not have type errors', async () => {
       await typeCheckFolder([__dirname, 'valid'], false);
-    });
+    }, 30000);
   })
 });
