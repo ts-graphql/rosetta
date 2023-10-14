5c35a1cac1bbc99fd27455994a6f447de0dfe35c
stephentuso <tusodev@gmail.com>
2023-10-13 23:35:20 -0400

    chore: remove checked in build files

--- packages/rosetta/src/util/braces.d.ts
+++
@@ -1,6 +0,0 @@
-type AddDepth<Depth extends string> = `${Depth}.`;
-type SubtractDepth<Depth extends string> = Depth extends `${infer Next}.` ? Next : never;
-type GetBraceContents<PreOpen extends string, PostOpen extends string, Depth extends string> = PreOpen extends `${infer PreClose}}${infer PostClose}` ? Depth extends AddDepth<''> ? PreClose : `${PreClose}}${InsideNextBraces<`${PostClose}{${PostOpen}`, SubtractDepth<Depth>>}` : Depth extends '' ? InsideNextBraces<PostOpen, AddDepth<Depth>> : `${PreOpen}{${InsideNextBraces<PostOpen, AddDepth<Depth>>}`;
-export type InsideNextBraces<Query extends string, Depth extends string = ''> = Query extends `${infer PreOpen}{${infer PostOpen}` ? GetBraceContents<PreOpen, PostOpen, Depth> : Depth extends '' ? '' : GetBraceContents<Query, '', Depth>;
-export type OutsideNextBraces<Query extends string> = Query extends `${infer Before}{${InsideNextBraces<Query>}}${infer After}` ? `${Before}${After}` : Query;
-export {};
--- packages/rosetta/src/util/general.d.ts
+++
@@ -1,7 +0,0 @@
-export type Maybe<T> = T | null | undefined;
-export type Whitespace = '\n' | ' ' | ',' | `#${string}\n`;
-export type AfterWhitespace<Query extends string> = Query extends `${Whitespace}${infer After}` ? AfterWhitespace<After> : Query;
-export type BeforeWhitespace<Query extends string> = Query extends `${infer Before}${Whitespace}${string}` ? BeforeWhitespace<Before> : Query;
-export type IsNonNullable<T> = T extends {} ? true : false;
-export type UntilFirst<Item extends string, Query extends string> = Query extends `${infer A}${Item}${string}` ? A : Query;
-export type AfterFirst<Item extends string, Query extends string> = Query extends `${string}${Item}${infer After}` ? After : '';
