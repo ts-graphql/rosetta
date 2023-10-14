a4026368ee3772a0587bf93558d3f42de04816e3
stephentuso <tusodev@gmail.com>
2023-10-13 21:41:14 -0400

    build: update config

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 if [ "${CIRCLE_BRANCH}" == "main" ]; then
-  npx lerna version --conventional-commits --create-release github -y
+  npx lerna version --conventional-commits --create-release github --force-git-tag -y
   npx lerna publish from-git -y
 fi
